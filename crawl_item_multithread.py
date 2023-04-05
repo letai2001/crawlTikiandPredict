4efc053451457e4db5ed80ae67a969874e2dace5
letai2001 <nguyenletai16122001@gmail.com>
2023-04-05 15:50:38 +0700

    'push'

--- crawl_item_multithread.py
+++ crawl_item_multithread.py
@@ -38,6 +38,75 @@ data = []
 
 MAX_RETRIES = 5
 number_of_threads = 8
+def find_rep_shop(driver):
+    for j in range(MAX_RETRIES): 
+        try:
+            rep_chat_ele = driver.find_element(By.CLASS_NAME, "item.chat") 
+            if rep_chat_ele is None:
+                x = "N/A"
+            else:
+                try:
+                    title_div = rep_chat_ele.find_element(By.CLASS_NAME, "title")
+                    span = title_div.find_element(By.TAG_NAME, "span")
+                    x = span.get_attribute("textContent")
+                    break
+                    # shop_follow_list.append(follow)
+                except:
+                    x = "N/A"
+                    break
+        except Exception as e:
+            print(f"Khong lây dc rep_chat , retrying ({j+1}/{MAX_RETRIES})...!")
+            if(j==4):
+                    x = "N/A"
+            sleep(1)
+
+    return x
+
+def find_follow_shop(driver):
+    for j in range(MAX_RETRIES): 
+        try:
+            shop_follow = driver.find_element(By.CLASS_NAME, "item.normal") 
+            if shop_follow is None:
+                x = 0
+                break
+            else:
+                try:
+                    title_div = shop_follow.find_element(By.CLASS_NAME, "title")
+                    span = title_div.find_element(By.TAG_NAME, "span")
+                    follow_text = span.get_attribute("textContent")
+                    x = humanfriendly.parse_size(follow_text, binary=True)
+                    # shop_follow_list.append(follow)
+                except:
+                    x = 0
+                break  # thoát vòng lặp nếu đã xác định được giá trị của bi
+        except Exception as e:
+            print(f"Khong lây dc shop_follow , retrying ({j+1}/{MAX_RETRIES})...!")
+            if(j==4):
+                    x  = 0
+            sleep(1)
+
+    return x
+
+def find_rate_shop(driver):
+    x = 0
+    for j in range(MAX_RETRIES): 
+        try:
+            item_review_elements = driver.find_elements(By.CLASS_NAME, "item.review")
+            if item_review_elements:
+                item_review = item_review_elements[0]
+                title_div = item_review.find_element(By.CLASS_NAME, "title")
+                span = title_div.find_element(By.TAG_NAME, "span")
+                rating_text = span.get_attribute("textContent")
+                match = re.search(r"\d+\.\d+", rating_text)
+                if match:
+                    x = float(match.group())
+                break  # thoát vòng lặp nếu đã xác định được giá trị của biến
+            else:
+                break  # thoát vòng lặp nếu không tìm thấy phần tử
+        except Exception as e: 
+            print(f"khong thay phan tu item_review , retrying ({j+1}/{MAX_RETRIES})...")
+            sleep(1)
+    return x
 def find_quantity_sold(driver ,css_name ):
     for j in range(MAX_RETRIES):            
             try: 
@@ -74,8 +143,6 @@ def find_ele(driver , class_name):
                     sleep(1.5*j)
     return x
 
-
-# Open the JSON file for reading
 def find_rating(driver , classname):    
     for j in range(MAX_RETRIES+1):
             try:
@@ -96,9 +163,12 @@ def find_rating(driver , classname):
                     driver.execute_script("window.scrollBy(0, {});".format(int(height * (0.4+j*0.1))))
                     sleep(2*j)
     return x
+
 lock = threading.Lock()
 visited_links_lock  = threading.Lock()
 visited_links = set()
+# Open the JSON file for reading
+
 try:
     with open('data_fix.json', 'r') as f:
         for line in f:
@@ -116,84 +186,13 @@ def get_data_from_link(links , lock , visited_links_lock):
                     price = find_ele(driver , "product-price__current-price")
                     discount = find_ele(driver , "product-price__discount-rate")
                     review_count = find_ele(driver , "number")
-                    # review_count = int(review_count.split()[1])
                     count_code = find_ele(driver , "coupon__text")
-                # count_code = int(count_code.split(" ")[0])
-                
                     sold_number = find_quantity_sold(driver, 'div[data-view-id="pdp_quantity_sold"].styles__StyledQuantitySold-sc-1u1gph7-2.exWbxD' )
-
-
-                    #rate_shop
-                    rating = 0
+                    rating = find_rate_shop(driver)
+                    follow = find_follow_shop(driver)
+                    rep_chat_text = find_rep_shop(driver)
                     
-                    for j in range(MAX_RETRIES): 
-                        try:
-                            item_review_elements = driver.find_elements(By.CLASS_NAME, "item.review")
-                            if item_review_elements:
-                                item_review = item_review_elements[0]
-                                title_div = item_review.find_element(By.CLASS_NAME, "title")
-                                span = title_div.find_element(By.TAG_NAME, "span")
-                                rating_text = span.get_attribute("textContent")
-                                match = re.search(r"\d+\.\d+", rating_text)
-                                if match:
-                                    rating = float(match.group())
-                                break  # thoát vòng lặp nếu đã xác định được giá trị của biến
-                            else:
-                                break  # thoát vòng lặp nếu không tìm thấy phần tử
-                        except Exception as e: 
-                            print(f"khong thay phan tu item_review , retrying ({j+1}/{MAX_RETRIES})...")
-                            sleep(1)
-                
-                    
-
-
-
-
-                    for j in range(MAX_RETRIES): 
-                        try:
-                            shop_follow = driver.find_element(By.CLASS_NAME, "item.normal") 
-                            if shop_follow is None:
-                                follow = 0
-                                break
-                            else:
-                                try:
-                                    title_div = shop_follow.find_element(By.CLASS_NAME, "title")
-                                    span = title_div.find_element(By.TAG_NAME, "span")
-                                    follow_text = span.get_attribute("textContent")
-                                    follow = humanfriendly.parse_size(follow_text, binary=True)
-                                    # shop_follow_list.append(follow)
-                                except:
-                                    follow = 0
-                                break  # thoát vòng lặp nếu đã xác định được giá trị của bi
-                        except Exception as e:
-                            print(f"Khong lây dc shop_follow , retrying ({j+1}/{MAX_RETRIES})...!")
-                            if(j==4):
-                                    follow  = 0
-                            sleep(1)
-
-                    #rep_chat
-                    for j in range(MAX_RETRIES): 
-                        try:
-                            rep_chat_ele = driver.find_element(By.CLASS_NAME, "item.chat") 
-                            if rep_chat_ele is None:
-                                rep_chat_text = "N/A"
-                            else:
-                                try:
-                                    title_div = rep_chat_ele.find_element(By.CLASS_NAME, "title")
-                                    span = title_div.find_element(By.TAG_NAME, "span")
-                                    rep_chat_text = span.get_attribute("textContent")
-                                    break
-                                    # shop_follow_list.append(follow)
-                                except:
-                                    rep_chat_text = "N/A"
-                                    break
-                        except Exception as e:
-                            print(f"Khong lây dc rep_chat , retrying ({j+1}/{MAX_RETRIES})...!")
-                            if(j==4):
-                                    rep_chat_text = "N/A"
-                            sleep(1)
                     height = driver.execute_script("return document.documentElement.scrollHeight")
-
                         # # Cuộn trang xuống 1/3 chiều cao của trang
                     driver.execute_script("window.scrollBy(0, {});".format(int(height * 0.2)))
                         # html = driver.find_element(By.TAG_NAME, 'html')
@@ -203,22 +202,9 @@ def get_data_from_link(links , lock , visited_links_lock):
                     driver.execute_script("window.scrollBy(0, {});".format(int(height * 0.4)))
 
                     sleep(4)
-                        # driver.execute_script("window.scrollBy(0, {});".format(int(height * 0.6)))
-                        # sleep(2)
-                     # driver.execute_script("window.scrollBy(0, {});".format(int(height * 0.7)))
                     number_image = find_ele(driver , "review-images__heading")
-                        
-                        # rating_point_ele = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "review-rating__point")))
-                        
                     rating_point = find_rating(driver, "review-rating__point")
-                        # x = driver.find_element(By.CLASS_NAME ,  "review-rating__point")
                    
-                
-                
-                    # link2.append(a)
-                    # new_product = {"link": link, "price": price, 'discount': discount, 'review_count': review_count,
-                    #         "count_code": count_code, "quantity_sold": sold_number, "rate_shop": rating, "shop_follow": follow,
-                    #         "rating_avarage": rating_point}
                     data.append({"link": link, "price": price, 'discount': discount, 'review_count': review_count,
                             "count_code": count_code, "quantity_sold": sold_number, "rate_shop": rating, "shop_follow": follow, "rep_chat":rep_chat_text , "number_image":number_image,
                             "rating_avarage": rating_point})
@@ -233,54 +219,24 @@ def get_data_from_link(links , lock , visited_links_lock):
                             f.write('\n')
 
         # df = pd.DataFrame(data, columns=['Link', 'Price', 'Discount', 'Number of Ratings', 'Number of Reviews', 'Store Rating', 'Number of Store Followers', 'Available Coupons', 'Average Rating'])
-        
-threads = []
+def main():
+    threads = []
 
-for i in range(number_of_threads):
-    start = i * (len(p_link) // number_of_threads)
-    end = (i + 1) * (len(p_link) // number_of_threads)
-    if i == number_of_threads - 1:
-        end = len(p_link)
-    thread_links = p_link[start:end]
-    t = threading.Thread(target=get_data_from_link, args=(thread_links ,lock , visited_links_lock ,))
-    threads.append(t)
+    for i in range(number_of_threads):
+        start = i * (len(p_link) // number_of_threads)
+        end = (i + 1) * (len(p_link) // number_of_threads)
+        if i == number_of_threads - 1:
+            end = len(p_link)
+        thread_links = p_link[start:end]
+        t = threading.Thread(target=get_data_from_link, args=(thread_links ,lock , visited_links_lock ,))
+        threads.append(t)
 
-# Bắt đầu chạy các thread
-for t in threads:  
-    t.start()
+    # Bắt đầu chạy các thread
+    for t in threads:  
+        t.start()
 
-# Đợi cho tất cả các thread hoàn thành công việc
-for t in threads:
-    t.join()
-
-
-
-# print(prices_list)
-# print(len(prices_list))
-# print(discount_list)
-# print(len(discount_list))
-# print(reivew_counts_list)
-# print(len(reivew_counts_list))
-
-# print(quantity_sold_list)
-# print(len(quantity_sold_list))
-
-# print(rate_shop_list)
-# print(len(rate_shop_list))
-
-
-# print(shop_follow_list)
-# print(len(shop_follow_list))
-
-# print(count_code_discount_list)
-# print(len(count_code_discount_list))
-
-# print(ratting_point_list)
-# print(len(ratting_point_list))
-
-
-
-
-# df1 = pd.DataFrame({ 'price': prices_list , 'discount': discount_list , 'review_count': reivew_counts_list , 'quantity_sold': quantity_sold_list , 'rate_shop': rate_shop_list , 'shop_follow' : shop_follow_list , 'count_code' : count_code_discount_list , 'rating_avarage' : ratting_point_list} )
-
-# df1.to_csv('all_item_balo_602_1200.csv', index=True)
+    # Đợi cho tất cả các thread hoàn thành công việc
+    for t in threads:
+        t.join()
+if __name__ == "__main__":
+    main()
